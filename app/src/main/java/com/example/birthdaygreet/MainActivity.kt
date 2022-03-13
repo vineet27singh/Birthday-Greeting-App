a44f64890d83de33ae5a1b38122bf479ac36ebf0
vineet27singh <singh08vineet@gmail.com>
2022-03-13 13:34:18 +0530

    Birthday greeting App

---
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.birthdaygreet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val NAME_EXTRA = "name_extra"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name = nameInput.editableText.toString().capitalize()
+
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
+        intent.putExtra(NAME_EXTRA, name)
+        startActivity(intent)
+    }
+}
